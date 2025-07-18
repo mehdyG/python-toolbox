b3914fd0ac858d674f6a86eb9a87585aa57cee0c
Mehdy Gooya <gooya.mehdy1365@gmail.com>
2025-07-18 14:41:45 +0000

    Input parameters in code eingeben
    to be flexible for other wind turbines

--- pyFAST/DLC1p2/Example_DLC1P2.py
+++ pyFAST/DLC1p2/Example_DLC1P2.py
@@ -15,16 +15,35 @@ import numpy as np
 def main():
     this_dir = os.path.dirname(os.path.abspath(__file__))
 
+    # Load existing TurbSim input:
+    filename = 'TurbSim_DLC1p2NREL5MW_Land.inp'
+    ts_in = FASTInputFile(filename)
+
+    # Change input parameters based on NREL 5MW 
+    ts_in['NumGrid_Z']      = 41            # Turbsim Guide: NumGrid_Z = GridHeight/mean_Chord
+    ts_in['NumGrid_Y']      = 41            # Turbsim Guide: NumGrid_Y = GridWidth/mean_Chord
+    ts_in['TimeStep']       = 0.05          # s 
+    ts_in['AnalysisTime']   = 630           # s 30 s over to avoid initial transient effects 
+    ts_in['UsableTime']     = 600           # s
+    ts_in['HubHt']          = 90            # 90 m, 
+    ts_in['GridHeight']     = 140           # GridHeight = 1.1 * Rotor diameter = 1.1 * 126 m =- 140 m
+    ts_in['GridWidth']      = 140           # GridWidth = 1.1 * Rotor diameter
+    ts_in['TurbModel']      = "IECKAI"      
+    ts_in['NIECstandard']   = "1-Ed3"   
+    ts_in['IECturbc']       = "B"           # IECturbc
+    ts_in['IEC_WindType']   = "NTM"         
+    ts_in['WindProfileType'] = "IEC"     
+    ts_in['RefHt']          = 90            # Hub Height
+
+
     # --- Prepare and Run TurbSim
     # Define parameters
-    URefs = np.arange(3, 26, 1)     # V_cutin = 3 m/s to V_cut_out 0 25 m/s [3,4,...,25]
+    URefs = np.arange(3, 26, 2)     # V_cutin = 3 m/s to V_cut_out 0 25 m/s [3,5,...,25]
     seeds = [1, 2, 3, 4, 5, 6]      # Different random seeds für DLC 1.2 
 
     for u in URefs:
         for seed in seeds:
-            # Load existing TurbSim input:
-            filename = 'TurbSim_DLC1p2NREL5MW_Land.inp'
-            ts_in = FASTInputFile(filename)
+            
 
             # Modify parameters
             ts_in['URef'] = u
